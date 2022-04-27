229df2030a5509de392bd74b81dbbbd984a7887c
yeskinaa <yeskina2022@gmail.com>
2022-04-27 07:03:51 -0700

    first commit

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const slides = document.querySelectorAll('.slide')
+function slidesPlagin(activeSlide = 0) {
+  const slides = document.querySelectorAll('.slide')
+
+slides[activeSlide].classList.add('active')
 
 for (const slide of slides) {
-   slide.addEventListener('click', () => {
-       clearActiveClasses()
-      slide.classList.add('active') 
-   })
+  slide.addEventListener('click', () => {
+      clearActiveClasses()
+
+     slide.classList.add('active') 
+  })
 }
 
 function clearActiveClasses() {
-    slides.forEach((slide) => {
-      slide.classList.remove('active')
-    })
+   slides.forEach((slide) => {
+     slide.classList.remove('active')
+   })
+ }
 }
+
+slidesPlagin(4)
